623728e6e9279005663355173f69b166f7aa2717
nightconcept <dark@nightconcept.net>
2024-02-08 06:05:12 -0800

    Remove yazi

--- config/yazi/files/disable-status-line.lua
+++
@@ -1,8 +0,0 @@
--- This plugin disables the useless (in my opinion) status line, see `https://yazi-rs.github.io/docs/tips#no-status-bar`
-
-function Status:render() return {} end
-
-local old_manager_render = Manager.render
-function Manager:render(area)
-	return old_manager_render(self, ui.Rect { x = area.x, y = area.y, w = area.w, h = area.h + 1 })
-end
